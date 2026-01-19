e82abbb0c33dd5fdb9ded2c71b279a257dc5e713
Foofoo_The_Guy <32585652+FoofooTheGuy@users.noreply.github.com>
2026-01-19 12:59:07 -0500

    remove attempts at big endian support
    don't want

--- lib/sources/helpers.cpp
+++ lib/sources/helpers.cpp
@@ -1,84 +1,112 @@
-#include <limits>
-#include <array>
+#include "helpers.hpp"
 
-#include "internal.hpp"
-#include "darc.hpp"
+std::string readUTF16str(std::ifstream* input, uint32_t offset) {
 
-/* helper functions are here to avoid clutter in darc.cpp */
-
-const uint32_t darc::DARC_MAGIC = 0x63726164; // "darc"
-const uint32_t darc::DARC_VERSION = 0x01000000;
-
-std::map<darc::return_code, std::string> darc::return_codes {
-	{OK, "No errors"},
-	{INVALID_MAGIC, "Magic is not the expected value"},
-	{INVALID_VERSION, "Version is not the expected value"},
-	{FAIL_OPEN, "Failed to open file"},
-	{NOT_OPEN, "The given stream is not open"},
-	{FAIL_READ, "Failed to read file"},
-	{FAIL_WRITE, "Failed to write to file"},
-	{TOO_SHORT,"The given size is too short"},
-	{TOO_LONG, "The given size is too short"},
-	{NO_MEM, "This program is out of memory to allocate"},
-	{INTERNAL, "Internal program error, this should never happen"},
-};
-
-darc::return_code darc::readU16(std::ifstream* file, uint16_t* value) {
-	file->read(
-		reinterpret_cast<char*>(value),
-		sizeof(uint16_t)
-	);
+	if (!input->is_open()) {
+		std::cout << "ERROR: file not open" << std::endl;
+		return "";
+	}
 	
-	if constexpr (std::endian::native == std::endian::big) {
-		if (file_endianess == endian::little) {
-			*value = byteswap<uint16_t>(*value);
+	input->seekg(offset);
+	
+	std::string line = "";
+	char byte;
+	while(input->read(reinterpret_cast<char*>(&byte), sizeof byte)) {
+		line += byte;		
+		if (byte == 0x00 && input->peek() == 0x00) { // UTF16 NULL terminator
+			input->get();
+			line += byte;
+			break;
 		}
 	}
-	else if constexpr (std::endian::native == std::endian::little) {
-		if (file_endianess == endian::big) {
-			*value = byteswap<uint16_t>(*value);
-		}
-	}
-	else { // apparently this is possible but I'm not going to support it
-		return return_code::INTERNAL;
-	}
-	return return_code::OK;
+	return line;
 }
 
-darc::return_code darc::readU32(std::ifstream* file, uint32_t* value) {
-	file->read(
-		reinterpret_cast<char*>(value),
-		sizeof(uint32_t)
-	);
+std::string UTF16toUTF8(const std::string& input) {
+	size_t outLen = 0;
+	size_t utf16length = input.size() / 2; // divide by 2 because it's a u8 size going into a u16 array
 	
-	if constexpr (std::endian::native == std::endian::big) {
-		if (file_endianess == endian::little) {
-			*value = byteswap<uint32_t>(*value);
-		}
-	}
-	else if constexpr (std::endian::native == std::endian::little) {
-		if (file_endianess == endian::big) {
-			*value = byteswap<uint32_t>(*value);
-		}
-	}
-	else { // apparently this is possible but I'm not going to support it
-		return return_code::INTERNAL;
-	}
-	return return_code::OK;
+	std::vector<uint16_t> utf16 = std::vector<uint16_t>(utf16length);
+	memcpy(utf16.data(), input.data(), input.size());
+	size_t utf8length = nnc_utf16_to_utf8(NULL, 0, utf16.data(), utf16length) + 1;
+	std::vector<uint8_t> utf8 = std::vector<uint8_t>(utf8length);
+	memset(utf8.data(), 0, utf8length);
+	
+	outLen = nnc_utf16_to_utf8(utf8.data(), utf8length, utf16.data(), utf16length);
+	std::string output(reinterpret_cast<char*>(utf8.data()), outLen);
+	
+	return output;
 }
 
-std::string darc::magic_to_string() {
-	std::string magicstr;
-	uint32_t magic = header.magic;
-	
-	if constexpr (std::endian::native == std::endian::big) {
-		magic = byteswap<uint32_t>(magic); // does this work?
-	}
-	
-	for (auto c : std::bit_cast<std::array<char,
-		sizeof(uint32_t)>>(magic))
+// junk stolen from nnc
+static void write_utf8(uint8_t *out, size_t outlen, size_t *outptr, uint32_t cp)
+{
+	if(cp < 0x80)
 	{
-		magicstr += c;
+		size_t n = *outptr + 1;
+		if(n < outlen)
+			out[*outptr] = cp;
+		*outptr = n;
 	}
-	return magicstr;
+	else if(cp < 0x800)
+	{
+		size_t n = *outptr + 2;
+		if(n < outlen)
+		{
+			out[*outptr + 0] = (cp >> 6)   | 0xC0;
+			out[*outptr + 1] = (cp & 0x3F) | 0x80;
+		}
+		*outptr = n;
+	}
+	else if(cp < 0x10000)
+	{
+		size_t n = *outptr + 3;
+		if(n < outlen)
+		{
+			out[*outptr + 0] = (cp >> 12)         | 0xE0;
+			out[*outptr + 1] = ((cp >> 6) & 0x3F) | 0x80;
+			out[*outptr + 2] = (cp & 0x3F)        | 0x80;
+		}
+		*outptr = n;
+	}
+	else if(cp < 0x110000)
+	{
+		size_t n = *outptr + 4;
+		if(n < outlen)
+		{
+			out[*outptr + 0] = (cp >> 18)          | 0xF0;
+			out[*outptr + 1] = ((cp >> 12) & 0x3F) | 0x80;
+			out[*outptr + 2] = ((cp >> 6) & 0x3F)  | 0x80;
+			out[*outptr + 3] = (cp & 0x3F)         | 0x80;
+		}
+		*outptr = n;
+	}
+	else { } /* invalid codepoint */
+}
+
+#define LE16(a) ((uint16_t) (a))
+
+/* should there be a BE version of this? */
+size_t nnc_utf16_to_utf8(uint8_t *out, size_t outlen, const uint16_t *in, size_t inlen)
+{
+	size_t outptr = 0;
+	for(size_t i = 0; i < inlen; ++i)
+	{
+		uint16_t p1 = LE16(in[i]);
+		if(p1 == '\0')
+			break; /* finished */
+		else if(p1 < 0xD800 || p1 > 0xE000)
+			write_utf8(out, outlen, &outptr, p1);
+		/* surrogate pair */
+		else
+		{
+			uint16_t p2 = LE16(in[i + 1]);
+			uint16_t w1 = p1 & 0x3FF;
+			uint16_t w2 = p2 & 0x3FF;
+			uint32_t cp = 0x10000 | (w1 << 10) | w2;
+			write_utf8(out, outlen, &outptr, cp);
+			++i; /* since we moved ahead 2 for the pair */
+		}
+	}
+	return outptr;
 }
