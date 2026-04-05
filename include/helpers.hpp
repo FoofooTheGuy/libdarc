76d9235af3ec5ad7e42cf0c4c4025bb6709e65ac
Foofoo_The_Guy <32585652+FoofooTheGuy@users.noreply.github.com>
2026-04-05 11:09:46 -0400

    update header too

--- include/helpers.hpp
+++ include/helpers.hpp
@@ -9,6 +9,8 @@
 
 std::string readUTF16str(std::ifstream* input, uint32_t offset);
 
+std::string UTF8toUTF16(const std::string input);
 std::string UTF16toUTF8(const std::string& input);
 
 size_t nnc_utf16_to_utf8(uint8_t *out, size_t outlen, const uint16_t *in, size_t inlen);
+size_t nnc_utf8_to_utf16(uint16_t *out, size_t outlen, const uint8_t *in, size_t inlen);
